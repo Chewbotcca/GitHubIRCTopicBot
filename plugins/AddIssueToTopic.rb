1fd680e0f8de6f34e9635eaa4b649ba72e1d7891
Chewsterchew <chew@chew.pw>
2017-10-12 16:06:40 -0500

    make this work

--- plugins/AddIssueToTopic.rb
+++ plugins/AddIssueToTopic.rb
@@ -1,12 +1,15 @@
 class AddIssueToTopic
   include Cinch::Plugin
 
-  match /[ChewbotccaIRC] Chewsterchew opened issue/, :use_prefix => false, :strip_colors => true, method: => :addissue
+  match '[ChewbotccaIRC] Chewsterchew opened issue', use_prefix: :false, strip_colors: :true
 
-  def addissue(m)
-    m.reply "You got it"
-    topicsplit = m.channel.topic.split(' ')
-    topcisplit[11] = topicsplit[11].to_i + 1
-    m.channel.topic=topicsplit.join(' ')
+  def execute(m)
+    if "#{m.user.user}@#{m.user.host}" == 'notifico@ChewCraft-p8h.k4l.199.198.IP' && m.channel.name == '#Chewbotcca'
+      topicsplit = m.channel.topic.split(' ')
+      topicsplit[10] = topicsplit[10].to_i + 1
+      m.channel.topic = topicsplit.join(' ')
+    else
+      m.reply 'YOU ARENT NOTIFICO!!'
+    end
   end
 end
