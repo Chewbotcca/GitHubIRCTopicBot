53d7afb71a0b6c35a60e9be226bae9e1b35af02b
Chewsterchew <chew@chew.pw>
2017-10-12 16:33:36 -0500

    Allow anyone to open and close issues to modify count

--- plugins/AddIssueToTopic.rb
+++ plugins/AddIssueToTopic.rb
@@ -1,15 +1,18 @@
 class AddIssueToTopic
   include Cinch::Plugin
 
-  match '[ChewbotccaIRC] Chewsterchew opened issue', use_prefix: :false, strip_colors: :true
+  match /\[IRC\] (.+)/, strip_colors: :true, use_prefix: :false
 
-  def execute(m)
-    if "#{m.user.user}@#{m.user.host}" == 'notifico@ChewCraft-p8h.k4l.199.198.IP' && m.channel.name == '#Chewbotcca'
-      topicsplit = m.channel.topic.split(' ')
-      topicsplit[10] = topicsplit[10].to_i + 1
-      m.channel.topic = topicsplit.join(' ')
-    else
-      m.reply 'YOU ARENT NOTIFICO!!'
+  def execute(m, args)
+    splitargs = args.split(' ')
+    if splitargs[1] = 'opened'
+      if "#{m.user.user}@#{m.user.host}" == 'notifico@ChewCraft-p8h.k4l.199.198.IP' && m.channel.name == '#Chewbotcca'
+        topicsplit = m.channel.topic.split(' ')
+        topicsplit[10] = topicsplit[10].to_i + 1
+        m.channel.topic = topicsplit.join(' ')
+      else
+        m.reply 'YOU ARENT NOTIFICO!!'
+      end
     end
   end
 end
--- plugins/RemoveIssueFromTopic.rb
+++ plugins/RemoveIssueFromTopic.rb
@@ -1,15 +1,18 @@
 class RemoveIssueFromTopic
   include Cinch::Plugin
 
-  match '[ChewbotccaIRC] Chewsterchew closed issue', use_prefix: :false, strip_colors: :true
+  match /\[IRC\] (.+)/, strip_colors: :true, use_prefix: :false
 
   def execute(m)
-    if "#{m.user.user}@#{m.user.host}" == 'notifico@ChewCraft-p8h.k4l.199.198.IP' && m.channel.name == '#Chewbotcca'
-      topicsplit = m.channel.topic.split(' ')
-      topicsplit[10] = topicsplit[10].to_i - 1
-      m.channel.topic = topicsplit.join(' ')
-    else
-      m.reply 'YOU ARENT NOTIFICO!!'
+    splitargs = args.split(' ')
+    if splitargs[1] = 'closed'
+      if "#{m.user.user}@#{m.user.host}" == 'notifico@ChewCraft-p8h.k4l.199.198.IP' && m.channel.name == '#Chewbotcca'
+        topicsplit = m.channel.topic.split(' ')
+        topicsplit[10] = topicsplit[10].to_i - 1
+        m.channel.topic = topicsplit.join(' ')
+      else
+        m.reply 'YOU ARENT NOTIFICO!!'
+      end
     end
   end
 end
