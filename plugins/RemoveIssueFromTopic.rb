abfeec54380781991bbabd5f933d93993b0d8b7a
Chewsterchew <chew@chew.pw>
2017-10-27 15:21:40 -0500

    now works with any channel in the channel config

--- plugins/RemoveIssueFromTopic.rb
+++ plugins/RemoveIssueFromTopic.rb
@@ -5,14 +5,16 @@ class RemoveIssueFromTopic
 
   def execute(m, args)
     splitargs = args.split(' ')
-    if splitargs[1] == 'closed' && splitargs[2] == 'issue'
-      if "#{m.user.user}@#{m.user.host}" == 'notifico@ChewCraft-p8h.k4l.199.198.IP' && m.channel.name == '#Chewbotcca'
-        topicsplit = m.channel.topic.split(' ')
-        location = topicsplit.index { |s| s.include?('Issues:') } + 1
-        topicsplit[location] = topicsplit[location].to_i - 1
-        m.channel.topic = topicsplit.join(' ')
-      else
-        m.reply 'YOU ARENT NOTIFICO!!'
+    if CONFIG['channels'].include?(m.channel.name)
+      if splitargs[1] == 'closed' && splitargs[2] == 'issue'
+        if "#{m.user.user}@#{m.user.host}" == 'notifico@ChewCraft-p8h.k4l.199.198.IP'
+          topicsplit = m.channel.topic.split(' ')
+          location = topicsplit.index { |s| s.include?('Issues:') } + 1
+          topicsplit[location] = topicsplit[location].to_i - 1
+          m.channel.topic = topicsplit.join(' ')
+        else
+          m.reply 'YOU ARENT NOTIFICO!!'
+        end
       end
     end
   end
