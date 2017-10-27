d301b83ad2a2da0553a77d4e6dc8779a54434573
Chewsterchew <chew@chew.pw>
2017-10-27 15:39:31 -0500

    Allow custom notifico! hostmask

--- plugins/RemoveIssueFromTopic.rb
+++ plugins/RemoveIssueFromTopic.rb
@@ -7,7 +7,7 @@ class RemoveIssueFromTopic
     splitargs = args.split(' ')
     if CONFIG['channels'].include?(m.channel.name)
       if splitargs[1] == 'closed' && splitargs[2] == 'issue'
-        if "#{m.user.user}@#{m.user.host}" == 'notifico@ChewCraft-p8h.k4l.199.198.IP'
+        if "#{m.user.user}@#{m.user.host}" == "notifico@#{CONFIG['notifico']}"
           topicsplit = m.channel.topic.split(' ')
           location = topicsplit.index { |s| s.include?('Issues:') } + 1
           topicsplit[location] = topicsplit[location].to_i - 1
