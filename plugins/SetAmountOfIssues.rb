cc13cd4becb4686746bbffbf5ac3fac196618160
Chewsterchew <chew@chew.pw>
2017-11-03 16:13:43 -0500

    [Fixes #2] It took a while, but you can finally set a channel's repo.

--- plugins/SetAmountOfIssues.rb
+++ plugins/SetAmountOfIssues.rb
@@ -2,6 +2,23 @@ class SetAmountOfIssues
   include Cinch::Plugin
 
   match /~issues (.+)/
+  match /~setrepo (.+)/, method: :setrepo
+
+  def setrepo(m, repo)
+    unless File.exist?('data.yaml')
+      File.new('data.yaml', 'w+')
+      exconfig = YAML.load_file('data.sample.yaml')
+      File.open('data.yaml', 'w') { |f| f.write exconfig.to_yaml }
+    end
+    data = YAML.load_file('data.yaml')
+    betterchannelname = m.channel.name[1..m.channel.name.length]
+    repo = repo.split('/')
+    author = repo[0]
+    reponame = repo[1]
+    require 'yaml'
+    data[betterchannelname] = "https://api.github.com/repos/#{author}/#{reponame}"
+    File.open('data.yaml', 'w') { |f| f.write data.to_yaml }
+  end
 
   def execute(m, args)
     if CONFIG['ownerhost'] == m.user.host
