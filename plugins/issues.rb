383205284f9dfef4fd38ae8f334a51bd1113a229
Chewsterchew <chew@chew.pw>
2017-11-04 17:31:57 -0500

    put that return thing I learned to good use

--- plugins/issues.rb
+++ plugins/issues.rb
@@ -1,33 +1,33 @@
 class Issues
   include Cinch::Plugin
 
-  match /\[IRC\] (.+)/, strip_colors: true
+  match /(.+)/, strip_colors: true
   match /~issues (.+)/, method: :issues
 
   def execute(m, args)
     splitargs = args.split(' ')
-    if CONFIG['channels'].include?(m.channel.name)
-      if splitargs[1] == 'opened' && splitargs[2] == 'issue'
-        if "#{m.user.user}@#{m.user.host}" == "notifico@#{CONFIG['notifico']}"
-          topicsplit = m.channel.topic.split(' ')
-          location = topicsplit.index { |s| s.include?('Issues:') } + 1
-          topicsplit[location] = topicsplit[location].to_i + 1
-          m.channel.topic = topicsplit.join(' ')
-        else
-          m.reply 'YOU ARENT NOTIFICO!!'
-        end
-      end
+    unless CONFIG['channels'].include?(m.channel.name)
+      m.reply "This channel isn't in the list of channels I'm authorized to modify. Please invite me. Leaving..."
+      Channel(m.channel).part
+      return
+    end
+    return unless splitargs[3] == 'issue'
+    unless "#{m.user.user}@#{m.user.host}" == "notifico@#{CONFIG['notifico']}"
+      m.reply 'Only a configured notifico! host can be used to modify the channel topic!'
+      return
+    end
+    if splitargs[2] == 'opened'
+      topicsplit = m.channel.topic.split(' ')
+      location = topicsplit.index { |s| s.include?('Issues:') } + 1
+      topicsplit[location] = topicsplit[location].to_i + 1
+      m.channel.topic = topicsplit.join(' ')
+    end
 
-      if splitargs[1] == 'closed' && splitargs[2] == 'issue'
-        if "#{m.user.user}@#{m.user.host}" == "notifico@#{CONFIG['notifico']}"
-          topicsplit = m.channel.topic.split(' ')
-          location = topicsplit.index { |s| s.include?('Issues:') } + 1
-          topicsplit[location] = topicsplit[location].to_i - 1
-          m.channel.topic = topicsplit.join(' ')
-        else
-          m.reply 'YOU ARENT NOTIFICO!!'
-        end
-      end
+    if splitargs[2] == 'closed'
+      topicsplit = m.channel.topic.split(' ')
+      location = topicsplit.index { |s| s.include?('Issues:') } + 1
+      topicsplit[location] = topicsplit[location].to_i - 1
+      m.channel.topic = topicsplit.join(' ')
     end
   end
 
